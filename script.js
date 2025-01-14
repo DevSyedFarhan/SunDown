816bd75707a311f48b82b7a608d3db959975c955
unknown <syedfarhangraphic@gmail.com>
2025-01-14 05:46:24 +0500

    commit updated!

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+let container = document.querySelector("#elem-container");
+let fixed = document.querySelector("#fixed-image");
+
+container.addEventListener("mouseenter", function(){
+    fixed.style.opacity = 1;
+})
+container.addEventListener("mouseleave", function(){
+    fixed.style.opacity = 0;
+})
+
+let elems = document.querySelectorAll(".elem");
+elems.forEach(function(e){
+    e.addEventListener("mouseenter", function(){
+        let image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
